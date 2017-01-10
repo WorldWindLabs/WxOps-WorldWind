7cea38ac92a226a075e8739990ec66032c4d5d31
Scott T Shipley <wxazygy@wxanalyst.com>
2017-01-09 21:35:09 -0500

    Update CameraControlWindow.java
    first demo comapi

--- src/WxOps/Worldwind/CameraControlWindow.java
+++ src/WxOps/Worldwind/CameraControlWindow.java
@@ -11,42 +11,46 @@ import javax.swing.JFrame;
 import java.awt.Dialog.ModalityType;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Calendar;
+import java.util.TimeZone;
 import javax.swing.*;
 
 /**
  *
- * @author Peter
+ * @author Peter, wxazygy 9 Jan 2017
  */
 public class CameraControlWindow extends javax.swing.JFrame {
 
     private int c;
     private WorldWindow wwd;
-
+    //public CameraControlWindow ccw;
+ 
     public CameraControlWindow(WorldWindow wwd) { // This section initialise the generated code.
         this.wwd = wwd;
         initComponents();
         if(c<1){
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);  //DISPOSE_ON_CLOSE
 //            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); This function will end the program 
         }
+        //ccw = this;
+        this.setTitle("Camera");
     }
 
-    
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();  //Lon
+        jLabel2 = new javax.swing.JLabel();  //Lat
+        jLabel3 = new javax.swing.JLabel();  //Alt
+        jLabel4 = new javax.swing.JLabel();  //Roll
+        jLabel5 = new javax.swing.JLabel();  //Tilt
+        jLabel6 = new javax.swing.JLabel();  //Azi
+        jLabel7 = new javax.swing.JLabel();  //FOV
+        jButton1 = new javax.swing.JButton();  //Get Cam
+        jButton2 = new javax.swing.JButton();  //Set Cam
         jCheckBox1 = new javax.swing.JCheckBox();
         jLabel8 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
@@ -86,8 +90,9 @@ public class CameraControlWindow extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
-
-        jCheckBox1.setText("Close on Set Camera");
+        
+        
+        jCheckBox1.setText("Start Clock");
         jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jCheckBox1ActionPerformed(evt);
@@ -205,13 +210,14 @@ public class CameraControlWindow extends javax.swing.JFrame {
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {                                           
 // If the checkbox is clicked, the counter 
-        if (c == 0) {
-            c = 0;
-            c++;
-        } else {
-            c = 0;
-        }
-
+  //      if (c == 0) {
+  //          c = 0;
+  //          c++;
+  //      } else {
+  //          c = 0;
+  //      }
+        Timer twx = new Timer(1000, new ClockListener());
+        twx.start();
     }                                          
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
@@ -243,7 +249,10 @@ public class CameraControlWindow extends javax.swing.JFrame {
     }                                        
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-
+        refresh();
+    } 
+    
+    private void refresh() {
         BasicOrbitView view = (BasicOrbitView) this.wwd.getView();
         view.setEyePosition(Position.fromDegrees(Double.parseDouble(jTextField2.getText()), Double.parseDouble(jTextField1.getText()), Double.parseDouble(jTextField3.getText())));
         view.setRoll(Angle.fromDegrees(Double.parseDouble(jTextField4.getText())));
@@ -251,8 +260,36 @@ public class CameraControlWindow extends javax.swing.JFrame {
         view.setHeading(Angle.fromDegrees(Double.parseDouble(jTextField6.getText())));
         view.setFieldOfView(Angle.fromDegrees(Double.parseDouble(jTextField7.getText())));
         this.wwd.redraw();
-    }                                        
+    }
+    
+    //cannot be called from static class (WatchDir)
+    public void jsetPOV(String sLon, String sLat, String sRng, String sRoll,
+            String sTilt, String sAzi, String sFOV, String sSpd) {
+        try {
+            jTextField1.setText(sLon); //lon
+            jTextField2.setText(sLat); //lat
+            //elevation OR range
+            jTextField3.setText(sRng);//double zwx = 19070000.0;
+            jTextField4.setText(sRoll);//tilt which is pitch in WWJ
+            jTextField5.setText(sTilt);//azimuth which is heading in WWJ
+            jTextField6.setText(sAzi);
+            jTextField7.setText(sFOV);//field of view
+            refresh();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+    }
+   
+    class ClockListener implements ActionListener {
 
+        public void actionPerformed(ActionEvent e) {
+            Calendar now = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+            String hh = String.format("%02d", now.get(Calendar.HOUR_OF_DAY));
+            String mm = String.format("%02d", now.get(Calendar.MINUTE));
+            String ss = String.format("%02d", now.get(Calendar.SECOND));
+            jCheckBox1.setText("" + hh + ":" + mm + ":" + ss + " Z");
+        }
+    } //ClockListener
     // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
