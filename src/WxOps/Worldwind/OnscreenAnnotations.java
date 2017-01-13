5ea23592c42db6fa596ba29592081b9f471a97c3
PC <PC@PC-PC>
2017-01-12 23:15:04 -0500

    Updated README Reworked the Camera Control Menu. -Changed how the menu is made -Old menu is avialable Reworked the Animation Help. -Changed how the menu is made Fixed Animation Date -Should now properly update when new dates are entered

---
+++ src/WxOps/Worldwind/OnscreenAnnotations.java
@@ -0,0 +1,34 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package WxOps.Worldwind;
+//
+///**
+// *
+// * @author PC
+// */
+//public class OnscreenAnnotations {
+// //   this.setupAnnotations();
+//}
+//
+//private void setupAnnotations(){
+//GlobeAnnotation ga;
+//// RenderableLayer rl = new RenderableLayer();
+//// rl.setName("Annotations (stand alone)");
+//// insertBeforeCompass(this.getWwd(), rl);
+//
+//
+//
+//            // Using a ScreenAnnotation
+//            ScreenAnnotation sa = new ScreenAnnotation("Fixed position annotation", new Point(20, 50));
+//            sa.getAttributes().setDefaults(defaultAttributes);
+//            sa.getAttributes().setCornerRadius(0);
+//            sa.getAttributes().setSize(new Dimension(200, 0));
+//            sa.getAttributes().setAdjustWidthToText(AVKey.SIZE_FIXED); // use strict dimension width - 200
+//            sa.getAttributes().setDrawOffset(new Point(100, 0)); // screen point is annotation bottom left corner
+//            sa.getAttributes().setHighlightScale(1);             // No highlighting either
+//            layer.addAnnotation(sa);
+//
+//}
